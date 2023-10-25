ed467e489139e959869f6c4cfb99946e931000fd
Bi-Longdt <longdt.21it@vku.udn.vn>
2023-10-25 07:08:54 +0700

    Complete

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const fs = require('fs');
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     fs.readFile('index.html', (err, data) => {
-    // fs.readFile('non_existent_file.html', (err, data) => { // Thêm lỗi ở đây
       if (err) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
         res.end('Internal Server Error');
